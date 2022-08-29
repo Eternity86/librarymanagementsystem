5637fd97bc9133ff9a1fdb59e49e09c0d92f6055
Aleksandr.Kostrenkov <aleksandr.kostrenkov@nordclan.com>
2022-08-29 10:11:52 +0300

    Initial commit

---
+++ src/test/java/ru/arbiter2008/librarymanagementsystem/LibrarymanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.arbiter2008.librarymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarymanagementsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
