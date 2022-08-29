da1f92cb131fbe200e614f316c088cc28428b256
Aleksandr.Kostrenkov <aleksandr.kostrenkov@nordclan.com>
2022-08-29 17:27:15 +0300

    implemented AuthorService methods

--- src/main/java/ru/arbiter2008/librarymanagementsystem/service/impl/AuthorServiceImpl.java
+++ src/main/java/ru/arbiter2008/librarymanagementsystem/service/impl/AuthorServiceImpl.java
@@ -1,44 +1,72 @@
 package ru.arbiter2008.librarymanagementsystem.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import ru.arbiter2008.librarymanagementsystem.exception.NotFoundException;
 import ru.arbiter2008.librarymanagementsystem.model.Author;
+import ru.arbiter2008.librarymanagementsystem.repository.AuthorRepository;
 import ru.arbiter2008.librarymanagementsystem.service.AuthorService;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class AuthorServiceImpl implements AuthorService {
 
+    private final AuthorRepository authorRepository;
+
     @Override
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     public List<Author> findAllAuthors() {
-        return null;
+        return authorRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     public Author findAuthorById(Long id) {
-        return null;
+        return authorRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(String.format("Author with ID %d not found", id)));
     }
 
     @Override
     public void createAuthor(Author author) {
-
+        authorRepository.save(author);
     }
 
     @Override
     public void updateAuthor(Author author) {
-
+        authorRepository.save(author);
     }
 
     @Override
     public void deleteAuthor(Long id) {
-
+        var author = authorRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(String.format("Author with ID %d not found", id)));
+        authorRepository.delete(author);
     }
 
     @Override
     public Page<Author> findPaginated(Pageable pageable) {
-        return null;
+        var pageSize = pageable.getPageSize();
+        var currentPage = pageable.getPageNumber();
+        var startItem = currentPage * pageSize;
+        List<Author> authorList;
+
+        if (findAllAuthors().size() < startItem) {
+            authorList = Collections.emptyList();
+        } else {
+            var toIndex = Math.min(startItem + pageSize, findAllAuthors().size());
+            authorList = findAllAuthors().subList(startItem, toIndex);
+        }
+
+        return new PageImpl<>(authorList, PageRequest.of(currentPage, pageSize), findAllAuthors().size());
     }
 
 }
