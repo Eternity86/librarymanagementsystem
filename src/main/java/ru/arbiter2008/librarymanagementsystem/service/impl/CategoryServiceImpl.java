d855847709ec61e8c54a5dc5cac92fc09223f6a1
Aleksandr.Kostrenkov <aleksandr.kostrenkov@nordclan.com>
2022-08-29 16:37:17 +0300

    implemented CategoryService methods

--- src/main/java/ru/arbiter2008/librarymanagementsystem/service/impl/CategoryServiceImpl.java
+++ src/main/java/ru/arbiter2008/librarymanagementsystem/service/impl/CategoryServiceImpl.java
@@ -1,37 +1,50 @@
 package ru.arbiter2008.librarymanagementsystem.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import ru.arbiter2008.librarymanagementsystem.exception.NotFoundException;
 import ru.arbiter2008.librarymanagementsystem.model.Category;
+import ru.arbiter2008.librarymanagementsystem.repository.CategoryRepository;
 import ru.arbiter2008.librarymanagementsystem.service.CategoryService;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
 
+    private final CategoryRepository categoryRepository;
+
     @Override
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     public List<Category> findAllCategories() {
-        return null;
+        return categoryRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     public Category findCategoryById(Long id) {
-        return null;
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(String.format("Category with ID %d not found", id)));
     }
 
     @Override
     public void createCategory(Category category) {
-
+        categoryRepository.save(category);
     }
 
     @Override
     public void updateCategory(Category category) {
-
+        categoryRepository.save(category);
     }
 
     @Override
     public void deleteCategory(Long id) {
-
+        var category = categoryRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(String.format("Category with ID %d not found", id)));
+        categoryRepository.delete(category);
     }
 
 }
