4d34b38d83904524ef3769446e8ce05e1fab6625
Aleksandr.Kostrenkov <aleksandr.kostrenkov@nordclan.com>
2022-08-29 17:34:49 +0300

    implemented PublisherService methods

--- src/main/java/ru/arbiter2008/librarymanagementsystem/service/impl/PublisherServiceImpl.java
+++ src/main/java/ru/arbiter2008/librarymanagementsystem/service/impl/PublisherServiceImpl.java
@@ -1,37 +1,49 @@
 package ru.arbiter2008.librarymanagementsystem.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import ru.arbiter2008.librarymanagementsystem.exception.NotFoundException;
 import ru.arbiter2008.librarymanagementsystem.model.Publisher;
+import ru.arbiter2008.librarymanagementsystem.repository.PublisherRepository;
 import ru.arbiter2008.librarymanagementsystem.service.PublisherService;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class PublisherServiceImpl implements PublisherService {
 
+    private final PublisherRepository publisherRepository;
+
     @Override
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     public List<Publisher> findAllPublishers() {
-        return null;
+        return publisherRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     public Publisher findPublisherById(Long id) {
-        return null;
+        return publisherRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(String.format("Publisher with ID %d not found", id)));
     }
 
     @Override
     public void createPublisher(Publisher publisher) {
-
+        publisherRepository.save(publisher);
     }
 
     @Override
     public void updatePublisher(Publisher publisher) {
-
+        publisherRepository.save(publisher);
     }
 
     @Override
     public void deletePublisher(Long id) {
-
+        var publisher = publisherRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(String.format("Publisher with ID %d not found", id)));
     }
 
 }
